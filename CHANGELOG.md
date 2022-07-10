eb4e53765db5fe663c16a36c7979a16269071205
Thiemo Hoffmann <20605452+theimo1221@users.noreply.github.com>
2022-07-10 17:51:52 +0200

    Some Refactoring

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,9 @@
   Placeholder for the next version (at the beginning of the line):
   ## **WORK IN PROGRESS**
 -->
+## **WORK IN PROGRESS**
+* (theimo1221) Refactoring
+
 ## 0.0.2-alpha.0 (2022-07-09)
 * (theimo1221) Added Basic Project Structure including Workflow and npm scripts
 * (theimo1221) Add retrieve Auth Token functionality
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import fetch, { Response } from 'node-fetch';
+import fetch, { HeaderInit, Response } from 'node-fetch';
 import { Agent } from 'https';
 
 export class NodeMerlinWrtApi {
@@ -14,7 +14,10 @@ export class NodeMerlinWrtApi {
     this._agent = new Agent({ rejectUnauthorized: !ignoreSSL });
     this._basicAuth = Buffer.from(`${this._username}:${this._password}`).toString('base64');
   }
-  public async getAuthToken(): Promise<string> {
+  public async getAuthToken(forceNew: boolean = false): Promise<string> {
+    if(!forceNew && this._authToken !== '') {
+      return this._authToken;
+    }
     const loginData: { [name: string]: string } = {
       current_page: 'Main_Login.asp',
       login_authorization: this._basicAuth,
@@ -73,16 +76,21 @@ export class NodeMerlinWrtApi {
     await fetch(`${this._routerAddress}/Logout.asp`, {
       agent: this._agent,
       method: 'GET',
-      headers: {
-        accept: 'application/json, text/javascript, */*; q=0.01',
-        'accept-language': 'de-DE,de;q=0.9,en-US;q=0.8,en;q=0.7',
-        'cache-control': 'no-cache',
-        referer: `${this._routerAddress}/index.asp`,
-        cookie: `asus_token=${this._authToken}; HttpOnly;`,
-        connection: 'close',
-      },
+      headers: await this.getDefaultHeader(),
       body: undefined,
     });
+    this._authToken = '';
+  }
+
+  private async getDefaultHeader(): Promise<HeaderInit> {
+    return {
+      accept: 'application/json, text/javascript, */*; q=0.01',
+      'accept-language': 'de-DE,de;q=0.9,en-US;q=0.8,en;q=0.7',
+      'cache-control': 'no-cache',
+      referer: `${this._routerAddress}/index.asp`,
+      cookie: `asus_token=${await this.getAuthToken()}; HttpOnly;`,
+      connection: 'close',
+    };
   }
 
   private static getFormData(dict: { [name: string]: string }): string {
